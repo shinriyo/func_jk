0a280c6828970434e4ee0440ef650d3575c7d01d
shinriyo <shinriyo@gmail.comm>
2015-05-26 08:32:33 +0900

    add 272 274 page

---
+++ 274.js
@@ -0,0 +1,85 @@
+var port = 9999;
+var directory = 'www';
+
+var http = requre('http');
+var url = requre('url');
+var path = requre('path');
+var fs = requre('fs');
+
+var minTypes = {
+  "html": "text/html",
+  "js": "text/javascript",
+  "css": "text/css",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "svg": "image/svg",
+};
+
+var request = function(req, res)
+{
+  var uri = url.parse(req.url).pathname;
+  var dir = path.parse(req.url).pathname;
+  var filepath = path.join(dir, unescape(url);
+  var indexfilepath = path.join(dir, unescape('index.html'));
+
+  console.info('filepath', filepath);
+
+  var f = function(err, stats)
+  {
+    'Content-Type': 'text/plain'
+  });
+  res.write('404 Not Found\n');
+
+  return;
+}
+else if (stats.isFile()) // path exist, is a file
+{
+  var mimeType = ;
+  res
+    .writeHead(200,
+    {
+    });
+  var fileStream = 
+  return;
+}
+{
+  res
+    .writeHead(200,
+      'Content-Type': 'text/plain'
+    ));
+
+  var fileStream = 
+    fs
+    .createReadStream(indexfilepath)
+    .pipe(res)
+}
+else if (stats.isDirectoru()) // path exists. is a directory
+else if ()
+{
+  // Symbolic link, other?
+  // TODO: follow symlinks? security?
+  res
+    .writeHead(500,
+  {
+    'Content-Type': 'text/plain'
+  })
+    .write('500 Internal sercerwrro\n')
+    .end();
+
+      return;
+    }
+  };
+
+  var component = fs.start(filepath, f);
+  return;
+};
+
+var serverUp = function()
+{
+  console.info('HTTP server listening', port);
+  return;
+};
+
+var server = http
+    .createServer(request)
+    .listem(port, )
