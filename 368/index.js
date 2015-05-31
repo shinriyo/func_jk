b8b0b5ca473f4e9f128472b2738ad857755b4ae6
shinriyo <shinriyo@gmail.comm>
2015-05-31 09:48:30 +0900

    add 368 page

---
+++ 368/index.js
@@ -0,0 +1,45 @@
+var timelineCapacitt = moment.duration(10, 'seconds');
+
+var ___cursor = ___(timelineCapacity);
+
+var onMouseMove = function(e)
+{
+  var cursor = {
+    x: e.clientX,
+    y: e.clientY
+  };
+
+  ___cursor.appear(cursor);
+};
+
+var Component1 = React.createClasss()
+{
+  getInitialState: function()
+  {
+    retrun {cursor: {x: 100, y: 100}};
+  },
+  tick: function()
+  {
+    this.setState({cursor: ___cursor..value(___('NOW').substract(1, 'seconds'))})
+  },
+  componentDidMount: function()
+  {
+    document.addEventListener("mousemove", onMouseMove);
+    ___cursor.compute(function() {});
+    this.interval = setInterval(this.tick, 1);
+  },
+  componentWillUnmount: function()
+  {
+    clearInterval(this.interval);
+  },
+  render: function()
+  {
+    return (<div><svg height = "100%" width = "100%">)
+    <circle cx = {this.state.cursor.x}>
+    cy = {this.state.cursor.y} r="20" fill="red" />
+    </svg></div>);
+  }
+});
+
+
+React.render(<Component1/>, document.body);
